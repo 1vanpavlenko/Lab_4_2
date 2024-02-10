5e8ebc3d67d63c678a3039550c42f8b4f0cbaec2
Ivan Pavlenko <ivan_pavlenko@knu.ua>
2024-02-10 15:27:11 +0200

    ddd

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from utils import *
 print(is_prime(9))
 
 print(fact(9))
-<<<<<<< Updated upstream
-=======
 
 print(is_pow_of_five(20))
 
 print(is_pow_of_two(16))
->>>>>>> Stashed changes
+
+print(is_pow_of_five(20))
+
